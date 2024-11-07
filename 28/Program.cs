fea5e878e1da2dcad4fe5ae54cb26e9daff745f3
hammad3082 <mohammed.hammad@vxceed.com>
2024-11-07 10:28:48 +0530

    28

---
+++ 28/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+
+int StrStr(string haystack, string needle)
+{
+    int result = haystack.IndexOf(needle);
+    return result;
+}
+
+Stopwatch stopwatch = new Stopwatch();
+
+stopwatch.Start();
+
+long memoryBefore = GC.GetTotalMemory(true);
+Console.WriteLine(StrStr("", ""));
+
+stopwatch.Stop();
+long memoryAfter = GC.GetTotalMemory(true);
+
+Console.WriteLine("Elapsed time in ticks: " + stopwatch.ElapsedTicks);
+Console.WriteLine("Elapsed time in milliseconds: " + stopwatch.Elapsed.TotalMilliseconds + " ms");
+Console.WriteLine("Memory used: " + (memoryAfter - memoryBefore) + " bytes");
