92b3f5ae1178c731f129f4437bf346d009195400
hammad3082 <mohammed.hammad@vxceed.com>
2024-11-08 22:05:55 +0530

    ArraySign

---
+++ 1822/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+int[] nums = { -1, -2, -3 , -4, 0};
+int result = ArraySign(nums);
+Console.WriteLine(result);
+int ArraySign(int[] nums)
+{
+    int count = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] == 0) { return 0; }
+        if (nums[i] < 0) { count++; }
+    }
+    if (count % 2 != 0)
+        return -1;
+    return 1;
+}
