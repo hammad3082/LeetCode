47773f37e364d819102668733323e244b280620c
hammad3082 <mohammed.hammad@vxceed.com>
2024-11-14 21:55:32 +0530

    Length of Last Word

---
+++ 58/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+int result = LengthOfLastWord("   fly me   to   the moon  ");
+Console.WriteLine(result);
+int LengthOfLastWord(string s)
+{
+    char[] chars = s.ToCharArray();
+    bool start = false;
+    int len = 0;
+   
+    for (int i = chars.Length - 1; i >= 0; i--)
+    {
+        if (chars[i] != ' ')
+            start = true;
+        if (start)
+        {
+            if (chars[i] == ' ')
+            {
+                return len;
+            }
+            len++;
+        }
+
+        
+    }
+    return chars.Length;
+}
