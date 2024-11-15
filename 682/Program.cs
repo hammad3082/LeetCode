fff65f5ed4e46c49bc3c0dd7f9140901a249e582
hammad3082 <mohammed.hammad@vxceed.com>
2024-11-15 22:38:24 +0530

    CalPoints

---
+++ 682/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+string[] operations = { "5", "2", "C", "D", "+" };
+
+int result = CalPoints(operations);
+int CalPoints(string[] operations)
+{
+    List<int> ints = new List<int>();
+    int index = -1;
+    int Sum = 0;
+    int rNum = 0;
+    for (int i=0;i< operations.Length;i++)
+    {
+        if (operations[i] == "+")
+        {
+            rNum = ints[index - 1] + ints[index];
+            ints.Add(rNum);
+            Sum += rNum;
+            index++;
+        }
+        else if (operations[i] == "D")
+        {
+            rNum = ints[index] * 2;
+            ints.Add(rNum);
+            Sum += rNum;
+            index++;
+        }
+        else if(operations[i] == "C") 
+        {
+            rNum = ints[index];
+            ints.Remove(rNum);
+            Sum -= rNum;
+            index--;
+        }
+        else
+        {
+            ints.Add(Convert.ToInt32(operations[i]));
+            index++;
+            Sum += ints[index];
+        }
+    }
+    //int sum = 0;
+    //foreach (int i in ints)
+    //{
+    //    sum += i;
+    //}
+    return Sum;
+}
+Console.WriteLine(result);
