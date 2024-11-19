1abf12d1a3f6b1d367ccfb487cedaa82bcc5f4e9
hammad3082 <mohammed.hammad@vxceed.com>
2024-11-19 22:40:37 +0530

    JudgeCircle

---
+++ 675/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Net.Http.Headers;
+
+bool resukt = Solution.JudgeCircle("LDRRLRUULR");
+Console.WriteLine(resukt);
+public class Solution
+{
+    public static bool JudgeCircle(string moves)
+    {
+        int num = 0;
+        for (int i = 0; i < moves.Length; i++)
+        {
+            switch (moves[i])
+            {
+                case 'U':
+                    num += 77;
+                    break;
+                case 'D':
+                    num -= 77;
+                    break;
+                case 'L':
+                    num += 99;
+                    break;
+                case 'R':
+                    num -= 99;
+                    break;
+
+            }
+        }
+        if (num == 0)
+            return true;
+        else
+            return false;
+    }
+
+}
+
