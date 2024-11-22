b4d1a43682c87db8e7cc40ffb899fe680ea0f568
hammad3082 <mohammed.hammad@vxceed.com>
2024-11-22 10:29:14 +0530

    MaximumWealth

---
+++ 1672/Program.cs
@@ -0,0 +1,28 @@
+﻿int[][] jagged_arr = new int[3][];
+
+// Initialize the elements
+jagged_arr[0] = new int[] { 1, 5};
+jagged_arr[1] = new int[] { 7,3 };
+jagged_arr[2] = new int[] { 3,5 };
+
+int result = MaximumWealth(jagged_arr);
+int MaximumWealth(int[][] accounts)
+{
+    int max = 0;
+    int sum = 0;
+    for (int i = 0; i < accounts.Length; i++)
+    {
+        sum = 0;
+        for (int j = 0; j < accounts[i].Length; j++)
+        {
+            sum += Convert.ToInt32(accounts[i][j]);
+            
+        }
+        if (max < sum)
+        {
+            max = sum; break;
+        }
+    }
+    return max;
+}
+Console.WriteLine(result);
