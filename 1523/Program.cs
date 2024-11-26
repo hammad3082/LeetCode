a3e4fd06eb465f0c6e1d2c668c5eb8975b1d9e64
hammad3082 <mohammed.hammad@vxceed.com>
2024-11-26 11:29:56 +0530

    CountOdds

---
+++ 1523/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+int result = CountOdds(8, 10);
+int CountOdds(int low, int high)
+{
+    bool x = true;
+    int num = low;
+    int result = 0;
+    while (x)
+    {
+        if (num % 2 != 0)
+        {
+            result++;
+        }
+        if(num == high)
+            x = false;
+        num++;
+    }
+    return result;
+}
+Console.WriteLine(result);
