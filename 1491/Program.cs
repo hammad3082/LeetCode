744e62fbfb3814b04e8d9e700767a8cb58e4cf01
hammad3082 <mohammed.hammad@vxceed.com>
2024-11-27 14:49:25 +0530

    Average

---
+++ 1491/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] salary = { 2, 3, 4, 5 };
+double result = Average(salary);
+double Average(int[] salary)
+{
+    Array.Sort(salary);
+    double sum = 0.0;
+    int s = 0;
+    for (int i = 1; i < salary.Length -1; i++)
+    {
+        sum += salary[i];
+        s++;
+    }
+
+    return Math.Round(sum/s,5);
+}
+Console.WriteLine(result);
