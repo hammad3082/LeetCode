c9c58326c892d77894b8701a1afcd0661319d3ed
hammad3082 <mohammed.hammad@vxceed.com>
2024-12-18 22:49:59 +0530

    27 not 29

---
+++ 29/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] nums = { 0, 1, 2, 2, 3, 0, 4, 2 };
+
+int res = RemoveElement(nums, 2);
+
+int RemoveElement(int[] nums, int val)
+{
+    int k = 0;
+    for (int i = 0; i < nums.Length;i++)
+    {
+        if (nums[i] == val)
+        {
+            k++;
+            nums[i] = 101;
+        }
+    }
+    Array.Sort(nums);
+    return k;
+}
