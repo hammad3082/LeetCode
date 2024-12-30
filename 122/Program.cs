967448083f0c60956cc383b65fc9e756a5f799e4
hammad3082 <mohammed.hammad@vxceed.com>
2024-12-30 22:11:56 +0530

    MaxProfit2

---
+++ 122/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] prices = { 7, 1, 5, 3, 6, 4 };
+
+int res = MaxProfit(prices);
+Console.WriteLine(res);
+int MaxProfit(int[] prices)
+{
+    int profit = 0, buyingPrice = prices[0], currentPrice = 0;
+    for (int i = 1; i < prices.Length; i++)
+    {
+        currentPrice = prices[i];
+        if (currentPrice < buyingPrice)
+            buyingPrice = currentPrice;
+        else if (currentPrice - buyingPrice > 0)
+        {
+            profit += currentPrice - buyingPrice;
+            buyingPrice = currentPrice;
+        }
+
+    }
+    return profit;
+}
