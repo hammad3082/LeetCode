bf7e21bdb0d44353bc9e3e71a8c0acdf37c200a3
hammad3082 <mohammed.hammad@vxceed.com>
2024-12-31 22:49:21 +0530

    CanJump1

---
+++ 55/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int[] nums = { 2, 0 };
+
+bool dd = CanJump(nums);
+Console.WriteLine(dd);
+bool CanJump(int[] nums)
+{
+    int len  = nums.Length;
+    int cur = 0,jump =0;
+    for (int i = 0; i < len; i++)
+    {
+        if (jump == 0&& i + 1 == len)
+            return true;
+
+        if (jump < 0)
+            return false;
+
+        cur = nums[i];
+
+        if(cur > jump) jump = cur;
+
+        if(i+ 1 + jump >= len)
+            return true;
+
+        jump--;
+    }
+
+    return false;
+}
