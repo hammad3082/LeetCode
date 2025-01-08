6af6788548454176c8241d711a0a76412a23d5b9
hammad3082 <mohammed.hammad@vxceed.com>
2025-01-08 23:39:56 +0530

    HIndex

---
+++ 274/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] citations = { 5, 0, 6, 1, 5 ,5};
+
+int res = HIndex(citations);
+Console.WriteLine(res);
+int HIndex(int[] citations)
+{
+    Array.Sort(citations, (x, y) => y.CompareTo(x));
+    int res = 0;
+    for (int i = 0; i < citations.Length; i++)
+    {
+        if (citations[i] >= i+1)
+            res++;
+        else
+            return res;
+    }
+    return res;
+}
