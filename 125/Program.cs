c331bb0ba478c23c4a50fd14a9dbe432279622b6
hammad3082 <mohammed.hammad@vxceed.com>
2025-02-03 23:34:09 +0530

    IsPalindrome

---
+++ 125/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Text;
+string dd = "0p";
+bool res = IsPalindrome(dd);
+Console.WriteLine(res);
+
+bool IsPalindrome(string s)
+{
+    StringBuilder procssedStr = new ();
+
+    foreach (char c in s)
+    {
+        if(char.IsLetterOrDigit(c))
+        {
+            procssedStr.Append(char.ToLower(c));
+        }
+    }
+    int len = procssedStr.Length;
+    
+    if( len > 0)
+    {
+        len--;
+        int hlen = len / 2;
+        for (int i = 0; i <= hlen; i++)
+        {
+            if (procssedStr[i] != procssedStr[len - i])
+                return false;
+        }
+    }
+
+    return true;
+}
