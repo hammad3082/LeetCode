e70c02b963805be8d71d8fb649029c0749332e29
hammad3082 <mohammed.hammad@vxceed.com>
2025-04-25 00:52:21 +0530

    ReverseString

---
+++ 344/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+ReverseString(new char[] { 'h','e','l' ,'t', 'o', '1'});
+void ReverseString(char[] s)
+{
+    int l = s.Length - 1;
+    int hl = l / 2;
+    char t;
+    for (int i = 0; i < hl; i++)
+    {
+        t = s[i];
+        //Console.WriteLine("t " + t);
+        s[i] = s[l - i];
+        //Console.WriteLine(s[i]);
+        s[l - i] = t;
+    }
+}
