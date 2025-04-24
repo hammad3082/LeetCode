147b08af1bad81d4fd3f076d8d8f3af1440e1e8b
hammad3082 <mohammed.hammad@vxceed.com>
2025-04-25 00:54:10 +0530

    FindNumbers

---
+++ 1295/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] nums = { 12, 345, 2, 6, 7896 };
+int res = FindNumbers(nums);
+
+int FindNumbers(int[] nums)
+{
+    int c = 0;
+    foreach (int n in nums)
+    {
+        //Console.WriteLine(n.ToString().Length);
+        if ((n.ToString().Length) % 2 == 0)
+            c++;
+    }
+    return c;
+}
