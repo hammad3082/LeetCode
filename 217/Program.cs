504fbe81484b2700e37b646b832dc54563a443b3
hammad3082 <mohammed.hammad@vxceed.com>
2025-04-28 21:14:34 +0530

    ContainsDuplicate

---
+++ 217/Program.cs
@@ -0,0 +1,15 @@
+﻿
+ContainsDuplicate(new int[] { 1, 2, 3, 1 });
+bool ContainsDuplicate(int[] nums)
+{
+
+    Dictionary<int, int> keyValues = new Dictionary<int, int>();
+
+    foreach (int num in nums)
+    {
+        if (keyValues.GetValueOrDefault(num) > 0)
+            return true;
+        keyValues[num] = keyValues.GetValueOrDefault(num) + 1;
+    }
+    return false;
+}
