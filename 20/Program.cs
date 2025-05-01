a648376a4213528e3bd558728aa5399b192eddd6
hammad3082 <mohammed.hammad@vxceed.com>
2025-05-02 01:29:01 +0530

    IsValid

---
+++ 20/Program.cs
@@ -0,0 +1,50 @@
+﻿
+Solution solution = new Solution();
+solution.IsValid("() {} [] {([])}");
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        char p;
+        foreach (char c in s)
+        {
+            if (c == '(' || c == '[' || c == '{')
+            {
+                stack.Push(c);
+            }
+            else if (c == 32)
+            {
+                continue;
+            }
+            else
+            {
+                if (stack.Count == 0)
+                    return false;
+                p = stack.Pop();
+                switch (c)
+                {
+                    case ')':
+                        if (p != '(')
+                            return false;
+                        break;
+                    case ']':
+                        if (p != '[')
+                            return false;
+                        break;
+                    case '}':
+                        if (p != '{')
+                            return false;
+                        break;
+                    default:
+                        return false;
+                        
+                }
+            }
+        }
+        if (stack.Count > 0)
+            return false;
+        return true;
+    }
+}
