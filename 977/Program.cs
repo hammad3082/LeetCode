80eb865ff0b00b14ff23d8ebf5f997947979556c
hammad3082 <mohammed.hammad@vxceed.com>
2025-05-06 23:10:52 +0530

    SortedSquares

---
+++ 977/Program.cs
@@ -0,0 +1,57 @@
+﻿
+Solution solution = new Solution();
+ int[] res = solution.SortedSquares(new int[] { 1 });
+
+int a = res[0];
+
+public class Solution
+{
+    public int[] SortedSquares(int[] nums)
+    {
+        int i = 0, a = 0;
+        int len = nums.Length;
+
+        int[] result = new int[len];
+
+        while (a < len)
+        {
+            if (nums[a] >= 0)
+            {
+                break;
+            }
+            a++;
+        }
+        
+        int b = a + 1;
+        
+        while (a >= 0 && b < len)
+        {
+            if (Math.Abs(nums[a]) < Math.Abs(nums[b]))
+            {
+                result[i] = nums[a] * nums[a];
+                a--;
+            }
+            else
+            {
+                result[i] = nums[b] * nums[b];
+                b++;
+            }
+            i++;
+        }
+
+        while (b < len)
+        {
+            result[i] = nums[b] * nums[b];
+            b++;
+            i++;
+        }
+
+        while (a >= 0)
+        {
+            result[i] = nums[a] * nums[a];
+            a--;
+            i++;
+        }
+        return result;
+    }
+}
