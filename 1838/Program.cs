9a62047650c426bba5b1182c0a3603adda300cc4
hammad3082 <mohammed.hammad@vxceed.com>
2025-05-20 23:08:07 +0530

    MaxFrequency

---
+++ 1838/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+    public int MaxFrequency(int[] nums, int k)
+    {
+        int res = 0;
+        int l = 0, r = 0;
+        long sum = 0;
+        Array.Sort(nums);
+        while (r < nums.Length)
+        {
+            sum += nums[r];
+
+            while ((long)nums[r] * (r - l + 1) > sum + k)
+            {
+                sum -= nums[l];
+                l++;
+            }
+            if (res < r - l + 1)
+                res = r - l + 1;
+            //res = Math.Max(res, r - l + 1);
+            r++;
+        }
+        return (int)res;
+    }
+}
