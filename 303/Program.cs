aa014211308d174ca7eda6a568d7974553a8ae15
hammad3082 <mohammed.hammad@vxceed.com>
2025-05-21 23:10:24 +0530

    NumArray

---
+++ 303/Program.cs
@@ -0,0 +1,37 @@
+﻿NumArray n = new NumArray(new int[] { -2, 0, 3, -5, 2, -1 });
+
+int r1 = n.SumRange(0, 2);
+int r2 = n.SumRange(2, 5);
+int r3 = n.SumRange(0, 5);
+
+Console.WriteLine();
+public class NumArray
+{
+    int[] array;
+    int[] sumArray;
+    public NumArray(int[] nums)
+    {
+        int sum = 0;
+        array = nums;
+        sumArray = new int[array.Length];
+        for (int i = 0; i <array.Length; i++)
+        {
+            sum += array[i];
+            sumArray[i] = sum;
+        }
+    }
+
+    public int SumRange(int left, int right)
+    {
+        if (left == 0)
+            return sumArray[right];
+        else
+         return sumArray[right] - sumArray[left - 1];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.SumRange(left,right);
+ */
