1e00ec8f9d5048efdb8006007f0284d55c8a7417
hammad3082 <mohammed.hammad@vxceed.com>
2025-06-18 22:32:02 +0530

    ContainsNearbyDuplicate

---
+++ 219/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+Solution sol = new Solution();
+
+sol.ContainsNearbyDuplicate(new int[] { 1, 2, 3, 1, 2, 3 }, 2);
+public class Solution
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        Dictionary<int, int> keyValues = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int prevoiusIndex = keyValues.GetValueOrDefault(nums[i]);
+
+            if (prevoiusIndex > 0)
+            {
+                if(i +1 - prevoiusIndex <= k)
+                    return true;
+            }
+            keyValues[nums[i]] = i + 1;
+        }
+        return false;
+    }
+}
