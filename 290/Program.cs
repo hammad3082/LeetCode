e9e670df1fa73f5a9c0632627ca57e9a9d5d66c4
hammad3082 <mohammed.hammad@vxceed.com>
2025-06-21 00:18:45 +0530

    WordPattern

---
+++ 290/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    public bool WordPattern(string pattern, string s)
+    {
+        string[] arr = s.Split(' ');
+
+        if (arr.Length != pattern.Length)
+            return false;
+
+        Dictionary<char, string> dict = new Dictionary<char, string>();
+
+        for (int i = 0; i < pattern.Length; i++)
+        {
+            if (dict.TryGetValue(pattern[i], out string res))
+            {
+                if (arr[i] != res)
+                    return false;
+            }
+            else
+            {
+                if (dict.ContainsValue(arr[i]))
+                    return false;
+
+                dict[pattern[i]] = arr[i];
+            }
+        }
+
+        return true;
+    }
+}
