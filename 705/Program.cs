aeec8f9c48020341c3e07e361ce61de76d8cb0de
hammad3082 <mohammed.hammad@vxceed.com>
2025-06-20 11:37:48 +0530

    MyHashSet

---
+++ 705/Program.cs
@@ -0,0 +1,35 @@
+﻿public class MyHashSet
+{
+    List<int>[] arrList;
+    int size = 251;
+    public MyHashSet()
+    {
+       
+        arrList = new List<int>[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            arrList[i] = new List<int>();
+        }
+    }
+
+    public void Add(int key)
+    {
+        int pos = key % size;
+        if(!arrList[pos].Contains(key))
+            arrList[pos].Add(key);
+    }
+
+    public void Remove(int key)
+    {
+        int pos = key % size;
+
+        arrList[pos].Remove(key);
+    }
+
+    public bool Contains(int key)
+    {
+        int pos = key % size;
+        return arrList[pos].Contains(key);
+    }
+}
