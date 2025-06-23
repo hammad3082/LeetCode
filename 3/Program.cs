c5da2a6dd1a14f6c4663b2956f352ede44ed9d20
hammad3082 <mohammed.hammad@vxceed.com>
2025-06-23 23:49:04 +0530

    LengthOfLongestSubstring

---
+++ 3/Program.cs
@@ -0,0 +1,67 @@
+﻿Solution sol = new Solution();
+int res = sol.LengthOfLongestSubstring("bbtablud");
+
+int a = 1 + 1;
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        List<char> previos = new List<char>();
+        int count = 0, maxCount = 0, removeCount = 0;
+        int i = 0;
+        while(i != s.Length)
+        {
+            int index = previos.IndexOf(s[i]);
+            if (index > -1)
+            {
+                do
+                {
+                    previos.RemoveAt(0);
+                    removeCount++;
+                }
+                while (previos.Contains(s[i]));
+
+                maxCount = Math.Max(count++, maxCount);
+
+                count -= removeCount;
+                removeCount = 0;
+                previos.Add(s[i]);
+            }
+            else
+            {
+                previos.Add(s[i]);
+                count++;
+            }
+            i++;
+        }
+
+        return Math.Max(count, maxCount);
+    }
+}
+public class Solution15
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        HashSet<char> previos = new HashSet<char>();
+        int count = 0, maxCount = 0;
+        foreach (char c in s)
+        {
+            if (previos.Contains(c))
+            {
+                previos = new HashSet<char> ();
+
+                maxCount = Math.Max(count,maxCount);
+
+                count = 1;
+                previos.Add(c);
+            }
+            else
+            {
+                previos.Add(c);
+                count++;
+            }
+        }
+
+        return Math.Max(count, maxCount);
+    }
+}
