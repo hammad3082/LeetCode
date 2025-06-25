77bb94eb5b891c08283e3ee3917fcd9e4a3be488
hammad3082 <mohammed.hammad@vxceed.com>
2025-06-25 22:49:21 +0530

    CanConstruct

---
+++ 383/Program.cs
@@ -0,0 +1,45 @@
+﻿public class Solution
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        int[] cArr = new int[26];
+
+        foreach (char c in magazine)
+        {
+            cArr[c - 'a']++;
+        }
+
+        foreach (char c in ransomNote)
+        {
+            if (cArr[c - 'a'] > 0)
+                cArr[c - 'a']--;
+            else
+                return false;
+        }
+
+        return true;
+    }
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        List<char> list = new List<char>(magazine);
+
+        //foreach (char c in ransomNote) 
+        //{ 
+        //    list.Add(c);
+        //}
+
+        foreach (char c in ransomNote)
+        {
+            if (list.Contains(c))
+            {
+                list.Remove(c);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
