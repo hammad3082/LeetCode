c23de2691f81f88d41c326d4677c2f58d5eb1f7d
hammad3082 <mohammed.hammad@vxceed.com>
2025-07-14 10:54:17 +0530

    Rotate

---
+++ 189/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Solution
+{
+    public void Rotate(int[] nums, int k)
+    {
+        int[] record = nums.ToArray();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int newIndex = (i + k) % nums.Length;
+
+            nums[newIndex] = record[i];
+        }
+    }
+}
