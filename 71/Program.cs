e2efa951cfacabbc07cb5fe5a29aa64878d8ce6c
hammad3082 <mohammed.hammad@vxceed.com>
2025-07-14 22:32:46 +0530

    SimplifyPath

---
+++ 71/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+    public string SimplifyPath(string path)
+    {
+        string[] strings = path.Split("/");
+        Stack<string> stack = new Stack<string>();
+        foreach (string s in strings)
+        {
+            if (s.Length > 0)
+            {
+                if(s == "..")
+                {
+                    if(stack.Count > 0)
+                        stack.Pop();
+                }
+                else if (s != ".")
+                {
+                    stack.Push(s);
+                }
+            }
+        }
+
+        return "/" + string.Join("/", stack.Reverse());
+    }
+}
