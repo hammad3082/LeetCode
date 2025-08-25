226e383cdc3bf5a06897eedcd83a6bf385f64692
hammad3082 <hammad3082@gmail.com>
2025-08-26 00:50:12 +0530

    Convert

---
+++ 6/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Solution
+{
+    public string Convert(string s, int numRows)
+    {
+        if (numRows <= 1 || numRows >= s.Length)
+            return s;
+
+        string[] array = new string[numRows];
+
+        int curRow = 0;
+
+        bool goingDown = false;
+
+        foreach (char c in s)
+        {
+            array[curRow] += c;
+
+            // flip direction at top / bottom
+            if (curRow == 0 || curRow == numRows - 1)
+                goingDown = !goingDown;
+
+            curRow += goingDown ? 1 : -1;
+
+            // if(curRow == numRows -1)
+            // {
+            //     curRow--;
+            //     goingDown = false;
+            // }
+            // else
+            // {
+            //     if(curRow == 0)
+            //         goingDown = true;
+            //     if (goingDown)
+            //         curRow++;
+            //     else
+            //         curRow--;
+            // }
+        }
+        return string.Join("", array);
+    }
+}
