cc6c62c73200bddd1a2b4f2006c46653e03a678d
hammad3082 <hammad3082@gmail.com>
2025-08-29 00:40:04 +0530

    SingleNumber

---
+++ 136/Program.cs
@@ -0,0 +1,19 @@
+﻿Solution sol = new Solution();
+
+//sol.SingleNumber(new int[] { 2, 2, 1 });
+sol.SingleNumber(new int[] { 4, 1, 2, 1, 2 });
+
+public class Solution
+{
+    public int SingleNumber(int[] nums)
+    {
+        int xor = 0;
+
+        foreach (int x in nums)
+        {
+            xor = xor ^ x;
+        }
+
+        return xor;
+    }
+}
