dbf3337d238e1dfed8ce54b9acae492e5584feaf
hammad3082 <hammad3082@gmail.com>
2025-08-30 00:40:46 +0530

    MissingNumber

---
+++ 268/Program.cs
@@ -0,0 +1,17 @@
+﻿Solution sol = new Solution();
+
+sol.MissingNumber(new int[] { 9,6,4,2,3,5,7,0,1 });
+
+public class Solution
+{
+    public int MissingNumber(int[] nums)
+    {
+        int xor = 0;
+        for (int x = 0; x< nums.Length; x++)
+        {
+            xor ^= x + 1;
+            xor ^= nums[x];
+        }
+        return xor;
+    }
+}
