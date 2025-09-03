3a034e34bef5343b0124d5af5c0453084c997096
hammad3082 <hammad3082@gmail.com>
2025-09-04 00:39:36 +0530

    FindWordsContaining

---
+++ 2942/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public IList<int> FindWordsContaining(string[] words, char x)
+    {
+        IList<int> res = new List<int>();
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            bool present = false;
+            string word = words[i];
+
+            foreach (char c in word)
+            {
+                if (c == x)
+                {
+                    present = true;
+                    break;
+                }
+            }
+
+            if (present)
+            {
+                res.Add(i);
+            }
+        }
+
+        return res;
+    }
+}
