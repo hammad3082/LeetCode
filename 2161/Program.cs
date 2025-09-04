79270bfd9f4b2c1dcc4f2fdbe38e031581fc3cfb
hammad3082 <hammad3082@gmail.com>
2025-09-05 00:32:46 +0530

    PivotArray

---
+++ 2161/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Solution
+{
+    public int[] PivotArray(int[] nums, int pivot)
+    {
+
+        List<int> Left = new List<int>();
+        List<int> Right = new List<int>();
+
+        int same = 0;
+
+        foreach (int x in nums)
+        {
+            if( x < pivot)
+            {
+                Left.Add(x);
+            }
+            else if( x > pivot) 
+            {
+                Right.Add(x);
+            }
+            else
+                same++;
+        }
+        while (same > 0)
+        {
+            Left.Add(pivot);
+            same--;
+        }
+        Left.AddRange(Right);
+;
+        return Left.ToArray();
+            ;
+    }
+}
