4dbbde13f4228e609270d19f0b31722f7cc58f9c
hammad3082 <hammad3082@gmail.com>
2025-09-07 00:12:19 +0530

    MinimumOperations

---
+++ 3190/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Solution Solution = new Solution();
+
+Solution.MinimumOperations(new int[] { 1, 2, 3, 4 });
+public class Solution
+{
+    public int MinimumOperations(int[] nums)
+    {
+        int sumRem = 0;
+
+        foreach (int num in nums)
+        {
+            int rem = num % 3;
+
+            if (rem == 2)// instead of adding 2 subtract 1
+                rem = 1;
+            sumRem += rem;
+        }
+        //foreach (int num in nums)
+        //{
+        //    sumRem += num >= 3 ? num % 3 : Math.Min(3 - num, num % 3);
+        //}
+        return sumRem;
+    }
+}
