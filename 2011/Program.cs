de5935d4eb512abca99a5d86003fe19fdf9d1f69
hammad3082 <hammad3082@gmail.com>
2025-09-11 23:53:20 +0530

    FinalValueAfterOperations

---
+++ 2011/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public int FinalValueAfterOperations(string[] operations)
+    {
+        int x = 0;
+
+        foreach (string operation in operations)
+        {
+            if (operation.Contains("-"))
+                x--;
+            else
+                x++;
+        }
+        return x;
+    }
+}
