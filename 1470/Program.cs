15582a2b0528076d6ad82859aed3b4cc23a91431
hammad3082 <hammad3082@gmail.com>
2025-09-13 00:27:41 +0530

    Shuffle

---
+++ 1470/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public int[] Shuffle(int[] nums, int n)
+    {
+        int[] res = new int[nums.Length];
+
+        int ri = 0;
+        for (int i = 0; i < n; i++)
+        {
+            res[ri++] = nums[i];
+
+            res[ri++] = nums[i + n];
+        }
+
+        return res;
+    }
+}
