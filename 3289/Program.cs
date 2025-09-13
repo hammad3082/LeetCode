67993a56ccf5152cfc19c2b2c9e8097be1a91ace
hammad3082 <hammad3082@gmail.com>
2025-09-13 23:02:49 +0530

    GetSneakyNumbers

---
+++ 3289/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public int[] GetSneakyNumbers(int[] nums)
+    {
+        int[] res = new int[2];
+        int resIndex = 0;
+
+        HashSet<int> visited = new HashSet<int>();
+        
+
+        foreach (int num in nums)
+        {
+            if (visited.Contains(num))
+            {
+                res[resIndex++] = num;
+            }
+            else
+            {
+                visited.Add(num);
+            }
+            if (resIndex == 2)
+                break;
+        }
+
+        return res;
+    }
+}
