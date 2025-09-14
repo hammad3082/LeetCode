0f6014404abbd44eb718c25e907cfdc0267fc7de
hammad3082 <hammad3082@gmail.com>
2025-09-14 23:22:00 +0530

    NumberOfEmployeesWhoMetTarget

---
+++ 2798/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public int NumberOfEmployeesWhoMetTarget(int[] hours, int target)
+    {
+        int count = 0;
+
+        foreach (int hour in hours)
+        {
+            if (hour >= target) count++;
+        }
+
+        return count;
+    }
+}
