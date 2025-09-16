a16e23132227cffad5c1204d8914afeb8200cf59
hammad3082 <hammad3082@gmail.com>
2025-09-16 23:25:29 +0530

    MinMovesToSeat

---
+++ 2037/Program.cs
@@ -0,0 +1,22 @@
+﻿Solution sol = new Solution();
+
+sol.MinMovesToSeat(new int[] { 3, 1, 5 }, new int[] { 2, 7, 4 });
+
+public class Solution
+{
+    public int MinMovesToSeat(int[] seats, int[] students)
+    {
+        int moves = 0;
+
+        Array.Sort(seats);
+        Array.Sort(students);
+
+        for(int i = 0; i< seats.Length; i++)
+        {
+            if (seats[i] != students[i])
+                moves += Math.Abs(seats[i] - students[i]);
+        }
+
+        return moves;
+    }
+}
