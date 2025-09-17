c1789559d0866011d9d6bd3eeede677ba5d8c76a
hammad3082 <hammad3082@gmail.com>
2025-09-17 22:25:48 +0530

    MostWordsFound

---
+++ 2114/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Solution
+{
+    public int MostWordsFound(string[] sentences)
+    {
+        int max = 0;
+
+        foreach (string sentence in sentences)
+            max = Math.Max(max, sentence.Split(' ').Length);
+
+        return max;
+    }
+}
