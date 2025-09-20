cda241e93ca963dbeb28d50522fc9c2d5e868352
hammad3082 <hammad3082@gmail.com>
2025-09-21 00:16:08 +0530

    ScoreOfString

---
+++ 3110/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public int ScoreOfString(string s)
+    {
+        int res = 0;
+
+        for (int i = 1; i < s.Length; i++)
+        {
+            int dif = s[i - 1] - s[i];
+
+            res += Math.Abs(dif);
+        }
+
+        return res;
+    }
+}
