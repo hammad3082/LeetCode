ffdd4eacf7d209f826eca9840dd32da5019aaaef
hammad3082 <hammad3082@gmail.com>
2025-09-23 19:28:47 +0530

    DefangIPaddr

---
+++ 1108/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+public class Solution
+{
+    public string DefangIPaddr(string address)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        foreach (char c in address)
+        {
+            if (c != '.')
+                stringBuilder.Append(c);
+            else stringBuilder.Append("[.]");
+        }
+
+        return stringBuilder.ToString();
+        //return address.Replace(".", "[.]");
+        //return string.Join("[.]",address.Split('.'));
+    }
+}
