1751366845ffc643408e21cdc48fb37f6ee4c0f6
hammad3082 <hammad3082@gmail.com>
2025-09-24 23:30:52 +0530

    NumJewelsInStones

---
+++ 771/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public int NumJewelsInStones(string jewels, string stones)
+    {
+        int res = 0;
+
+        HashSet<char> values = new (jewels);
+
+        foreach (char c in stones) {
+            if (values.Contains(c))
+                res++;
+        }
+
+        return res;
+    }
+}
