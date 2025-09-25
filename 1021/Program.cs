44b6491ae367fe0d44ff0f3fb02a3ea495de6ed4
hammad3082 <hammad3082@gmail.com>
2025-09-25 22:43:20 +0530

    RemoveOuterParentheses

---
+++ 1021/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+Solution sol = new Solution();
+
+sol.RemoveOuterParentheses("(()())(())");
+
+public class Solution
+{
+    public string RemoveOuterParentheses(string s)
+    {
+        StringBuilder ss = new StringBuilder();
+
+        int c = 1;
+
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (c > 1 || (s[i] == '(' && c > 0))
+                ss.Append(s[i]);
+           
+            if (s[i] == '(')
+                c++;
+            else
+                c--;
+        }
+
+        return ss.ToString();
+    }
+}
