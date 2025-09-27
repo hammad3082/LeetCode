a5f8d05fbadebd69f90fc5677ed002209e56539d
hammad3082 <hammad3082@gmail.com>
2025-09-28 00:22:55 +0530

    Interpret

---
+++ 1678/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+public class Solution
+{
+    public string Interpret(string command)
+    {
+        return command.Replace("()", "o").Replace("(al)", "al");
+    }
+
+    public string Interpret_1(string command)
+    {
+        StringBuilder res = new StringBuilder(command.Length);
+
+        for (int i = 0; i < command.Length; i++)
+        {
+            if (command[i] == 'G')
+                res.Append('G');
+
+            if (command[i] == '(')
+            {
+                if (command[i + 1] == ')')
+                {
+                    res.Append('o');
+                    i++;
+                }
+                else
+                {
+                    res.Append("al");
+                    i = i + 3;
+                }
+            }
+        }
+        return res.ToString();
+    }
+}
