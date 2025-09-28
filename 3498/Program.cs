eba5ead9becdcb19b40a4ba999c2316bf067d0d9
hammad3082 <hammad3082@gmail.com>
2025-09-29 00:21:54 +0530

    ReverseDegree

---
+++ 3498/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Solution
+{
+    public int ReverseDegree(string s)
+    {
+        int res = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            int charToInt = 26 - (s[i] - 97);
+            res += (charToInt * (i + 1));
+        }
+        return res;
+    }
+}
