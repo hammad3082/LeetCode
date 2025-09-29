ff87d55d905002c1db45e985bdc182bafbb4ab04
hammad3082 <hammad3082@gmail.com>
2025-09-29 23:01:58 +0530

    TruncateSentence

---
+++ 1816/Program.cs
@@ -0,0 +1,20 @@
+﻿Solution sol = new Solution();
+sol.TruncateSentence("Hello how are you Contestant", 4);
+
+public class Solution
+{
+    public string TruncateSentence(string s, int k)
+    {
+        int count = 0, i = 0;
+        for (; i < s.Length; i++)
+        {
+            if (s[i] == ' ')
+                count++;
+
+            if (count == k)
+                break;
+        }
+
+        return s.Substring(0, i);
+    }
+}
