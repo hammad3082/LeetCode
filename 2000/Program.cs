dfdba86b3529cf396facadc90a346f382c8e1bd2
hammad3082 <hammad3082@gmail.com>
2025-10-01 00:21:38 +0530

    ReversePrefix

---
+++ 2000/Program.cs
@@ -0,0 +1,38 @@
+﻿Solution sol = new Solution();
+
+sol.ReversePrefix("abcd", 'z');
+
+public class Solution
+{
+    public string ReversePrefix(string word, char ch)
+    {
+        int firstIndex = -1;
+
+        //firstIndex = word.IndexOf(ch);
+        for (int i = 0; i < word.Length; i++)
+        {
+            if (word[i] == ch)
+            {
+                firstIndex = i;
+                break;
+            }
+        }
+        if (firstIndex >= 0)
+        {
+            char[] chars = word.ToCharArray();
+
+            Array.Reverse(chars, 0, firstIndex + 1);
+
+            //for (int j = 0; j <= firstIndex / 2; j++)
+            //{
+            //    char temp = chars[j];
+            //    chars[j] = chars[firstIndex - j];
+            //    chars[firstIndex - j] = temp;
+            //}
+
+            return string.Join("", chars);
+        }
+        else
+            return word;
+    }
+}
