5bc03df73db809528ed89cf2d4ae65536e54e610
hammad3082 <hammad3082@gmail.com>
2025-10-01 22:52:14 +0530

    RearrangeArray

---
+++ 2149/Program.cs
@@ -0,0 +1,48 @@
+﻿public class Solution
+{
+    public int[] RearrangeArray(int[] nums)
+    {
+        int posIndex = 0;
+        int negIndex = 1;
+        int[] res = new int[nums.Length];
+
+        foreach (int num in nums)
+        {
+            if (num > 0)
+            {
+                res[posIndex] = num;
+                posIndex += 2;
+            }
+            else
+            {
+                res[negIndex] = num;
+                negIndex += 2;
+            }
+        }
+        return res;
+    }
+    public int[] RearrangeArray_1(int[] nums)
+    {
+        var pos = new Queue<int>();
+        var neg = new Queue<int>();
+
+        foreach (int i in nums)
+        {
+            if(i >= 0) 
+                pos.Enqueue(i);
+            else 
+                neg.Enqueue(i);
+        }
+
+        int j = -1;
+        while (pos.Count > 0 || pos.Count > 0)
+        {
+            if(pos.Count > 0)
+                nums[++j] = pos.Dequeue();
+            if (neg.Count > 0)
+                nums[++j] = neg.Dequeue();
+        }
+
+        return nums;
+    }
+}
