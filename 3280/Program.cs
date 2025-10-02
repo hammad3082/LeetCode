25a68242b09ea0530cca575fc3748aa8c46170c9
hammad3082 <hammad3082@gmail.com>
2025-10-02 23:59:23 +0530

    ConvertDateToBinary

---
+++ 3280/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public string ConvertDateToBinary(string date)
+    {
+        string[] parts = date.Split('-');
+
+        int year = int.Parse(parts[0]);
+        int month = int.Parse(parts[1]);
+        int day = int.Parse(parts[2]);
+
+        string yearBinary = Convert.ToString(year, 2);
+        string monthBinary = Convert.ToString(month, 2);
+        string dayBinary = Convert.ToString(day, 2);
+
+        return $"{yearBinary}-{monthBinary}-{dayBinary}";
+    }
+}
