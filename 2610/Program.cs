d78ec8bab9d769f524e554da179c46f60cf4d7ad
hammad3082 <hammad3082@gmail.com>
2025-10-03 23:12:09 +0530

    FindMatrix

---
+++ 2610/Program.cs
@@ -0,0 +1,43 @@
+﻿public class Solution
+{
+    public IList<IList<int>> FindMatrix(int[] nums)
+    {
+        var freqMap = new Dictionary<int, int>();
+        foreach (var num in nums)
+        {
+            if (freqMap.ContainsKey(num))
+            {
+                freqMap[num]++;
+            }
+            else
+            {
+                freqMap[num] = 1;
+            }
+        }
+
+        int maxFreq = 0;
+        foreach (var count in freqMap.Values)
+        {
+            maxFreq = Math.Max(maxFreq, count);
+        }
+
+        var result = new List<IList<int>>();
+        for (int i = 0; i < maxFreq; i++)
+        {
+            result.Add(new List<int>());
+        }
+
+        foreach (var kvp in freqMap)
+        {
+            int num = kvp.Key;
+            int count = kvp.Value;
+
+            for (int i = 0; i < count; i++)
+            {
+                result[i].Add(num);
+            }
+        }
+
+        return result;
+    }
+}
