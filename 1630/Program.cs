9c2dc3a0ddb3ccaf038a0e13fe4a64890079358b
hammad3082 <hammad3082@gmail.com>
2025-10-04 23:55:42 +0530

    CheckArithmeticSubarrays

---
+++ 1630/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Solution
+{
+    public IList<bool> CheckArithmeticSubarrays(int[] nums, int[] l, int[] r)
+    {
+        var ans = new List<bool>();
+        for (int i = 0; i < l.Length; i++)
+        {
+            int[] arr = new int[r[i] - l[i] + 1];
+            for (int j = 0; j < arr.Length; j++)
+            {
+                arr[j] = nums[l[i] + j];
+            }
+
+            ans.Add(check(arr));
+        }
+
+        return ans;
+    }
+
+    public bool check(int[] arr)
+    {
+        Array.Sort(arr);
+
+        int diff = arr[1] - arr[0];
+
+        for (int i = 2; i < arr.Length; i++)
+        {
+            if (arr[i] - arr[i - 1] != diff)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
