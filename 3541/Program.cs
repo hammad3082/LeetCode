8ec4eeb3fcc78c30bd3b0aba52d3c6246455e28c
hammad3082 <hammad3082@gmail.com>
2025-10-05 22:29:56 +0530

    MaxFreqSum

---
+++ 3541/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int MaxFreqSum(string s)
+    {
+        int Con = 0, Vow = 0;
+        Dictionary<char, int> Freq = new Dictionary<char, int>();
+        
+        foreach (char c in s)
+        {
+            if (!Freq.ContainsKey(c)) 
+                Freq[c] = 0;
+            
+            Freq[c]++;
+        }
+
+        foreach (var kv in Freq)
+        {
+            if ("aeiou".Contains(kv.Key))
+                Vow = Math.Max(Vow, kv.Value);
+            else
+                Con = Math.Max(Con, kv.Value);
+        }
+
+        return Con + Vow;
+    }
+}
