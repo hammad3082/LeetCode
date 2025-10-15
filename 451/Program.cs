f3e705d79d50547da54e2735911fabd943321c89
hammad3082 <hammad3082@gmail.com>
2025-10-16 00:37:01 +0530

    FrequencySort

---
+++ 451/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Solution
+{
+    public string FrequencySort(string s)
+    {
+        return new string(
+            s.GroupBy(c => c)
+             .OrderByDescending(g => g.Count())
+             .SelectMany(g => Enumerable.Repeat(g.Key, g.Count()))
+             .ToArray()
+        );
+    }
+}
