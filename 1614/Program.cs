55febbd6596290e2dcc924df56c41032d710ba91
hammad3082 <hammad3082@gmail.com>
2025-10-16 22:23:37 +0530

    MaxDepth

---
+++ 1614/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public int MaxDepth(string s)
+    {
+        int maxDepth = 0;
+
+        int currentDepth = 0;
+
+        foreach (char c in s)
+        {
+            if (c == '(')
+            {
+                currentDepth++;
+            }
+            else if (c == ')') 
+                currentDepth--;
+
+            maxDepth = Math.Max(maxDepth, currentDepth);
+        }
+
+        return maxDepth;
+    }
+}
