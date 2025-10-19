61a71606e43607edd0d4782d45bf524c3eb82916
hammad3082 <hammad3082@gmail.com>
2025-10-19 23:31:42 +0530

    BalancedStringSplit

---
+++ 1221/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Solution sol = new Solution();
+sol.BalancedStringSplit("LLLLLLR");
+public class Solution
+{
+    public int BalancedStringSplit(string s)
+    {
+        int res = 0, cnt = 0;
+        foreach (char c in s)
+        {
+            cnt += c == 'L' ? 1 : -1;
+            if (cnt == 0) ++res;
+        }
+        return res;
+    }
+}
