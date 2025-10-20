26c38f2e5b7f36b790ff79322cca99d6d16e71fe
hammad3082 <hammad3082@gmail.com>
2025-10-20 23:41:58 +0530

    DecodeMessage

---
+++ 2325/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+public class Solution
+{
+    public string DecodeMessage(string key, string message)
+    {
+        var substitutionMap = new Dictionary<char, char>();
+
+        char nextDecodedChar = 'a';
+
+        foreach (char keyChar in key)
+        {
+            if (keyChar != ' ')
+            {
+                if (!substitutionMap.ContainsKey(keyChar))
+                {
+                    substitutionMap.Add(keyChar, nextDecodedChar);
+                    nextDecodedChar++;
+                }
+            }
+        }
+
+        var decodedMessage = new StringBuilder(message.Length);
+
+        foreach (char messageChar in message)
+        {
+            if (messageChar == ' ')
+            {
+                decodedMessage.Append(' ');
+            }
+            else
+            {
+                if (substitutionMap.TryGetValue(messageChar, out char decodedChar))
+                {
+                    decodedMessage.Append(decodedChar);
+                }
+            }
+        }
+
+        return decodedMessage.ToString();
+    }
+}
