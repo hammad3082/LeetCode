9bdb0d4e97603df86dfcc49dc8ecdef34c448af2
hammad3082 <hammad3082@gmail.com>
2025-10-21 23:11:23 +0530

    RestoreString

---
+++ 1528/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public string RestoreString(string s, int[] indices)
+    {
+        char[] chars = new char[s.Length];
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            chars[indices[i]] = s[i];
+        }
+        
+        return new string(chars);
+        //return string.Concat(chars);
+    }
+}
