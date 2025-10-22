4e57e915fdfce239bddea0d73f0da6936370d61f
hammad3082 <hammad3082@gmail.com>
2025-10-23 00:27:09 +0530

    reverseBits

---
+++ 190/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+Solution n = new Solution();
+n.reverseBits(011);
+public class Solution
+{
+    public int reverseBits(int n)
+    {
+        int reversed = 0;
+
+        for (int i = 0; i < 32; i++)
+        {
+            reversed <<= 1;
+
+            int lastBit = n & 1;
+
+            reversed |= lastBit;
+
+            n >>= 1;
+        }
+
+        return reversed;
+    }
+}
