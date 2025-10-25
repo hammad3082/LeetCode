59b58f8530d704c84a20003070a5fc0bafbc26e9
hammad3082 <hammad3082@gmail.com>
2025-10-25 23:50:50 +0530

    CheckIfPangram

---
+++ 1832/Program.cs
@@ -0,0 +1,21 @@
+﻿Solution sol = new Solution();
+sol.CheckIfPangram("thequickbrownfoxjumpsoverthelazydog");
+public class Solution
+{
+    public bool CheckIfPangram(string sentence)
+    {
+        var visited = new HashSet<char>();
+
+        foreach (var word in sentence)
+        {
+            if (visited.Count == 26)
+                return true;
+
+            visited.Add(word);
+        }
+
+        if(visited.Count == 26)
+            return true;
+        return false;
+    }
+}
