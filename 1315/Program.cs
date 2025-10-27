ba09b3e419964c8e658cca87adc97883c9b988ea
hammad3082 <hammad3082@gmail.com>
2025-10-28 00:12:57 +0530

    CountAsterisks

---
+++ 1315/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public int CountAsterisks(string s)
+    {
+        bool insideBar = false;
+        int count = 0;
+
+        foreach (char c in s)
+        {
+            if (c == '|')
+            {
+                insideBar = !insideBar;
+            }
+            else if (c == '*' && !insideBar)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
