87309a07d4f887d735cb62e87f08444e9c064efa
hammad3082 <hammad3082@gmail.com>
2025-10-30 00:39:39 +0530

    BuildArray

---
+++ 1920/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public int[] BuildArray(int[] nums)
+    {
+        int[] res = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            //int index = nums[i];
+
+            res[i] = nums[nums[i]];
+        }
+
+        return res;
+    }
+}
