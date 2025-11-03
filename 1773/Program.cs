e07591547a0fbcf51c6bfb00ef7f124e2b6b9e31
hammad3082 <hammad3082@gmail.com>
2025-11-04 00:46:48 +0530

    CountMatches

---
+++ 1773/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue)
+    {
+        int index = 0, count = 0;
+
+        if (ruleKey == "color")
+            index = 1;
+        if (ruleKey == "name")
+            index = 2;
+
+        foreach (var item in items)
+        {
+            if (item[index] == ruleValue)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
