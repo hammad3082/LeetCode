1edbe368bd6fbf353b318839700dab7e94b5ba45
hammad3082 <hammad3082@gmail.com>
2025-11-06 00:27:17 +0530

    StableMountains

---
+++ 3285/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    // A Mountain is StableMountain if its preceding mountan's height is greater than threshold.
+    public IList<int> StableMountains(int[] height, int threshold)
+    {
+        List<int> list = new List<int>();
+
+        for (int i = 1; i < height.Length; i++)
+        {
+            if (height[i - 1] > threshold)
+                list.Add(i);
+        }
+
+        return list;
+    }
+}
