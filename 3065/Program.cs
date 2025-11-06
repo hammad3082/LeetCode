17e3445e0cd86310976c321ee276a56dd3f4bee7
hammad3082 <hammad3082@gmail.com>
2025-11-07 00:19:25 +0530

    MinOperations

---
+++ 3065/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public int MinOperations(int[] nums, int k)
+    {
+        int res = 0;
+
+        foreach (int i in nums)
+        {
+            if (i < k)
+                res++;
+        }
+
+        return res;
+    }
+}
