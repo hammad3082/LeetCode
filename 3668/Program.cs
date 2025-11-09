e7164b2268ccb1911068e03714b1afa9ff63abf8
hammad3082 <hammad3082@gmail.com>
2025-11-10 00:53:03 +0530

    RecoverOrder

---
+++ 3668/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Solution
+{
+    public int[] RecoverOrder(int[] order, int[] friends)
+    {
+        int[] result = new int[friends.Length];
+
+        HashSet<int> friendSet = new HashSet<int>(friends);
+
+        int index = 0;
+
+        foreach (int num in order)
+        {
+            if (friendSet.Contains(num))
+            {
+                result[index++] = num;
+            }
+        }
+
+        return result;
+    }
+    public int[] RecoverOrder_Tp(int[] order, int[] friends)
+    {
+        var chars = new bool[order.Length];
+        int iter = 0;
+
+        for (int i = 0; i < friends.Length; i++)
+            chars[friends[i] - 1] = true;
+
+        foreach (var item in order)
+            if (chars[item - 1])
+                friends[iter++] = item;
+
+        return friends;
+    }
+}
