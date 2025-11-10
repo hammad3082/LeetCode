acdb9ddbd52b4accb2b91e701da49274b44e9ba7
hammad3082 <hammad3082@gmail.com>
2025-11-10 23:12:05 +0530

    LargestAltitude

---
+++ 1732/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Solution sol = new Solution();
+
+sol.LargestAltitude(new int[] { -4, -3, -2, -1, 4, 3, 2 });
+public class Solution
+{
+    public int LargestAltitude(int[] gain)
+    {
+        // Cause The biker starts his trip on point 0 with altitude equal 0
+        int CurrentAltitude = 0;
+
+        int MaxAltitude = CurrentAltitude;
+
+        for (int i = 0; i < gain.Length; i++)
+        {
+            CurrentAltitude += gain[i];
+
+            if (CurrentAltitude > MaxAltitude)
+                MaxAltitude = CurrentAltitude;
+        }
+
+        return MaxAltitude;
+    }
+}
