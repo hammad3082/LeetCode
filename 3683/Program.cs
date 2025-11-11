bcfa6380884940962d4dab4b9b2f89e9c612e582
hammad3082 <hammad3082@gmail.com>
2025-11-12 00:54:31 +0530

    EarliestTime

---
+++ 3683/Program.cs
@@ -0,0 +1,20 @@
+﻿Solution sol = new Solution();
+sol.EarliestTime(new int[][] { new int[] { 1, 6 }, new int[] { 2, 3 } });
+
+public class Solution
+{
+    public int EarliestTime(int[][] tasks)
+    {
+        int minTime = int.MaxValue, time = 0;
+
+        foreach (int[] task in tasks)
+        {
+            time = task[0] + task[1];
+
+            if (time < minTime)
+                minTime = time;
+        }
+
+        return minTime;
+    }
+}
