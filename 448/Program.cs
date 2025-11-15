9f548ae67dec36f3cfdedbbad7a4b2e862301abd
hammad3082 <hammad3082@gmail.com>
2025-11-16 00:42:23 +0530

    FindDisappearedNumbers

---
+++ 448/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public IList<int> FindDisappearedNumbers(int[] nums)
+    {
+        List<int> result = new List<int>();
+
+        HashSet<int> hashSet = new HashSet<int>(nums);
+
+
+        for (int i = 1; i <= nums.Length; i++)
+        {
+            if (!hashSet.Contains(i))
+                result.Add(i);
+        }
+
+        return result;
+    }
+}
