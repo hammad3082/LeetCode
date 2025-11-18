ef47866c5fcb4dc08750730a561b22ff313bdacd
hammad3082 <hammad3082@gmail.com>
2025-11-19 00:10:35 +0530

    IsAcronym

---
+++ 2828/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public bool IsAcronym(IList<string> words, string s)
+    {
+        if(words.Count != s.Length) 
+            return false;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (!words[i].StartsWith(s[i]))
+                return false;
+            //    if (words[i][0] != s[i])
+            //        return false;
+        }
+        return true;
+    }
+}
