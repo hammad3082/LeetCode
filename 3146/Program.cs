1bf3c96aa4b25e4f0f2345a781b7d627fe20116d
hammad3082 <hammad3082@gmail.com>
2025-11-22 00:37:08 +0530

    FindPermutationDifference

---
+++ 3146/Program.cs
@@ -0,0 +1,44 @@
+﻿Solution sol = new Solution();
+sol.FindPermutationDifference("abcde", "edbac");
+
+public class Solution
+{
+    public int FindPermutationDifference(string s, string t)
+    {
+        int res = 0;
+
+        Dictionary<char,int> p = new Dictionary<char,int>();
+
+        for (int i = 0; i < s.Length; i++) 
+        { 
+            p[s[i]] = i;
+        }
+
+        for (int i = 0; i < t.Length; i++)
+        {
+            res += Math.Abs(p[t[i]] - i);
+        }
+
+        return res;
+    }
+    public int FindPermutationDifference_2(string s, string t) //alt
+    {
+        int res = 0;
+
+        int[] arr = new int[28];
+
+        Dictionary<char, int> p = new Dictionary<char, int>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            arr[s[i] - 'a'] = i;
+        }
+
+        for (int i = 0; i < t.Length; i++)
+        {
+            res += Math.Abs(arr[t[i] - 'a'] - i);
+        }
+
+        return res;
+    }
+}
