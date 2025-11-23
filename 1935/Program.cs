cafb3ea1a6cb166b956d9fa79fae52fe9722a67d
hammad3082 <hammad3082@gmail.com>
2025-11-24 00:51:30 +0530

    CanBeTypedWords

---
+++ 1935/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public int CanBeTypedWords(string text, string brokenLetters)
+    {
+        int res = 0;
+        string[] words = text.Split(' ');
+
+        foreach (string word in words)
+        {
+            bool canType = true;
+            //HashSet<char> charSet = new HashSet<char>(word);
+
+            foreach (char c in brokenLetters)
+            {
+                if (word.Contains(c))
+                {
+                    canType = false;
+                    break;
+                }
+            }
+
+            if (canType)
+                res++;
+        }
+        return res;
+    }
+}
