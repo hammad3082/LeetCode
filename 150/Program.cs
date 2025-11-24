64c44b8f256b18e3e0e6d0534eaf9cb7fa6ca4ac
hammad3082 <hammad3082@gmail.com>
2025-11-25 00:27:40 +0530

    EvalRPN

---
+++ 150/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public int EvalRPN(string[] tokens)
+    {
+        Stack<int> stack = new Stack<int>();
+        Dictionary<string, Func<int, int, int>> operation = new();
+
+        operation.Add("+", (x, y) => x + y);
+        operation.Add("*", (x, y) => x * y);
+
+        // first to be popped is the second number, so first param is actually the selond number
+        operation.Add("-", (y, x) => x - y);
+        operation.Add("/", (y, x) => x / y);
+
+        foreach(string t in tokens)
+        {
+            if (operation.ContainsKey(t))
+            {
+                stack.Push(operation[t](stack.Pop(), stack.Pop()));
+            }
+            else
+                stack.Push(Convert.ToInt32(t));
+        }
+
+        return stack.Peek();
+    }
+}
