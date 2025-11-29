20153de4bbc657c4410b3f728bed2cacb62d2390
hammad3082 <hammad3082@gmail.com>
2025-11-30 00:42:28 +0530

    RemoveDuplicates

---
+++ 1047/Program.cs
@@ -0,0 +1,26 @@
+﻿Solution sol = new Solution();
+sol.RemoveDuplicates("abbaca");
+
+public class Solution
+{
+    public string RemoveDuplicates(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char c in s)
+        {
+            if (stack.Count > 0 &&  stack.Peek() == c)
+            {
+                stack.Pop();
+            }
+            else
+                stack.Push(c);
+        }
+
+        char[] res = new char[stack.Count];
+        for (int i = res.Length - 1; i >= 0; i--)
+            res[i] = stack.Pop();
+
+        return new string(res);
+    }
+}
