5f7c0e6d009712048fd53035fbb6d46b7a42e524
hammad3082 <hammad3082@gmail.com>
2025-12-01 00:55:22 +0530

    MinLength

---
+++ 2696/Program.cs
@@ -0,0 +1,27 @@
+﻿Solution sol = new Solution();
+sol.MinLength("ABFCACDB");
+
+public class Solution
+{
+    public int MinLength(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char c in s)
+        {
+            if (stack.Count > 0 && (c == 'B' || c == 'D'))
+            {
+                char pre = stack.Peek();
+
+                if((pre == 'A' && c == 'B') || (pre == 'C' && c == 'D'))
+                    stack.Pop();
+                else
+                    stack.Push(c);
+            }
+            else
+                stack.Push(c);
+        }
+
+        return stack.Count;
+    }
+}
