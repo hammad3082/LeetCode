79e6bdc4e3d5b86c2854e2f4d50957ca5358d4ac
hammad3082 <hammad3082@gmail.com>
2025-12-04 22:44:47 +0530

    SortSentence

---
+++ 1894/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public string SortSentence(string s)
+    {
+
+        string[] words = s.Split(" ");
+        string[] result = new string[words.Length];
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            string word = words[i];
+
+            int index = Convert.ToInt32(word[word.Length - 1].ToString()) - 1;
+
+            result[index] = word.Substring(0, word.Length - 1);
+        }
+
+        return String.Join(" ", result);
+    }
+}
