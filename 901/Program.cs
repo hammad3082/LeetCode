ba5289eaf616b7ba0d6d6cccfb98d655a2718f39
hammad3082 <hammad3082@gmail.com>
2025-12-07 00:08:53 +0530

    StockSpanner

---
+++ 901/Program.cs
@@ -0,0 +1,30 @@
+﻿public class StockSpanner
+{
+
+    private Stack<(int price, int span)> stack;
+
+    public StockSpanner()
+    {
+        stack = new Stack<(int, int)>();
+    }
+
+    public int Next(int price)
+    {
+        int span = 1;
+
+        while (stack.Count > 0 && stack.Peek().price <= price)
+        {
+            span += stack.Pop().span;
+        }
+
+        stack.Push((price, span));
+
+        return span;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.Next(price);
+ */
