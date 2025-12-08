fd740d826e9461453894fc0819fe6aadc71d864a
hammad3082 <hammad3082@gmail.com>
2025-12-09 00:17:52 +0530

    NumberOfPairs

---
+++ 3162/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int NumberOfPairs(int[] nums1, int[] nums2, int k)
+    {
+        int res = 0;
+
+        foreach (int i in nums1)
+        {
+            foreach (int j in nums2)
+            {
+                if (i % (j * k) == 0)
+                {
+                    res++;
+                }
+            }
+        }
+
+        return res;
+    }
+}
