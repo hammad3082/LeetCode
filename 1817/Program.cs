f9a2c4057b64ae9b20859adacc012f4aa0b2fe2a
hammad3082 <hammad3082@gmail.com>
2025-12-09 23:51:11 +0530

    FindingUsersActiveMinutes

---
+++ 1817/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int[] FindingUsersActiveMinutes(int[][] logs, int k)
+    {
+        int[] res = new int[k];
+
+        Dictionary<int,HashSet<int>> userIdTimes = new Dictionary<int,HashSet<int>>();
+
+        foreach (int[] log in logs)
+        {
+            if(userIdTimes.ContainsKey(log[0]))
+                userIdTimes[log[0]].Add(log[1]);
+            else
+                userIdTimes[log[0]] = new HashSet<int>() { log[1] };
+        }
+
+        foreach(var Counts in userIdTimes.Values)
+        {
+            res[Counts.Count - 1]++;
+        }
+
+        return res;
+    }
+}
