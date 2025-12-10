cc34baa5267e8b2b86db6d0fb29b2d1acdcb1255
hammad3082 <hammad3082@gmail.com>
2025-12-10 23:34:50 +0530

    WateringPlants

---
+++ 2079/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+    public int WateringPlants(int[] plants, int capacity)
+    {
+        int waterInCan = capacity;
+        int steps = 0;
+
+        for (int i = 0; i < plants.Length; i++)
+        {
+            if (waterInCan < plants[i])
+            {
+                // i to go back and 1 to come back and 1 to actually go n water or move to current plant index
+                steps += (i + i + 1);
+                waterInCan = capacity - plants[i];
+            }
+            else
+            {
+                steps++;
+                waterInCan -= plants[i];
+            }
+        }
+
+        return steps;
+    }
+}
