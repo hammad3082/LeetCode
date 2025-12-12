d7842b929bfca2788d3fd5a130027606e589a91b
hammad3082 <hammad3082@gmail.com>
2025-12-13 00:51:46 +0530

    ArithmeticTriplets

---
+++ 2367/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int ArithmeticTriplets(int[] nums, int diff)
+    {
+        int res = 0, len = nums.Length;
+
+        for (int i = 0; i < len; i++)
+        {
+            for (int j = i + 1; j < len; j++)
+            {
+                for (int k = j + 1; k < len; k++)
+                {
+                    if (nums[k] - nums[j] == diff && nums[j] - nums[i] == diff)
+                        res++;
+                }
+            }
+        }
+
+        return res;
+    }
+}
