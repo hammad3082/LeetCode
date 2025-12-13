a7ab4a619e9540bbf84df887ef66413f5f8cc649
hammad3082 <hammad3082@gmail.com>
2025-12-13 21:03:23 +0530

    MakeGood

---
+++ 1544/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public string MakeGood(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char c in s)
+        {
+            if (stack.Count > 0 && Math.Abs(c - stack.Peek()) == 32)
+            {
+                stack.Pop();
+            }
+            else
+                stack.Push(c);
+        }
+
+        char[] res = new char[stack.Count];
+        for (int i = res.Length - 1; i >= 0; i--)
+            res[i] = stack.Pop();
+
+        return new string(res);
+    }
+}
