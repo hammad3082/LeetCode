2bccd4662010e9746159da88ad4bdd60dac7d1b2
hammad3082 <hammad3082@gmail.com>
2025-12-16 00:21:26 +0530

    SortPeople

---
+++ 2418/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Solution
+{
+    public string[] SortPeople(string[] names, int[] heights)
+    {
+        string[] res = new string[names.Length];
+
+        Dictionary<int, string> heightNames = new Dictionary<int, string>();
+
+        for (int i = 0; i < heights.Length; i++)
+        {
+            heightNames.Add(heights[i], names[i]);
+        }
+
+        Array.Sort(heights);
+
+        int ind = 0;
+        for (int i = heights.Length - 1; i >= 0; i--)
+        {
+            res[ind] = heightNames[heights[i]];
+            ind++;
+        }
+
+        return res;
+    }
+    public string[] SortPeople_ot(string[] names, int[] heights)
+    {
+        Array.Sort(heights, names);
+        var name = names.ToList<string>();
+        name.Reverse();
+        return name.ToArray();
+    }
+}
