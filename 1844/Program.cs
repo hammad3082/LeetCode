c881924dce359c7bbc267324f1b2893caa7e634b
hammad3082 <hammad3082@gmail.com>
2025-12-17 01:02:21 +0530

    ReplaceDigits

---
+++ 1844/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public string ReplaceDigits(string s)
+    {
+        char[] result = s.ToCharArray();
+
+        for (int i = 1; i < s.Length; i += 2)
+        {
+            result[i] = (char)(s[i - 1] + (s[i] - '0'));
+        }
+
+        return new string(result);
+    }
+}
