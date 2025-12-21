0c2f4d53028eec12cad98e2f9527f2288e3c01b6
hammad3082 <hammad3082@gmail.com>
2025-12-22 00:52:14 +0530

    RemoveTrailingZeros

---
+++ 2710/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public string RemoveTrailingZeros_TS(string num)
+    {
+        return num.TrimEnd('0');
+    }
+    public string RemoveTrailingZeros(string num)
+    {
+        int i = num.Length - 1;
+        for (; i >= 0; i--)
+        {
+            if(num[i] != '0')
+                break;
+        }
+
+        return num.Substring(0, i + 1);
+    }
+}
