dfff82d9d4a0e06a801bc8ebdfd8dad77f3261e5
hammad3082 <hammad3082@gmail.com>
2026-01-03 00:16:07 +0530

    MinCosts

---
+++ 3502/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public int[] MinCosts(int[] cost)
+    {
+        int min = cost[0];
+
+        for (int i = 1; i < cost.Length; i++)
+        {
+            if (cost[i] > min)
+                cost[i] = min;
+            else
+                min = cost[i];
+        }
+
+        return cost;
+    }
+}
