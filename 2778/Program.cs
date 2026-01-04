d8c675a337797dd72de7d7ef2ca014a0b0357da1
hammad3082 <hammad3082@gmail.com>
2026-01-05 00:16:38 +0530

    SumOfSquares

---
+++ 2778/Program.cs
@@ -0,0 +1,18 @@
+﻿Solution sol = new Solution();
+sol.SumOfSquares(new int[] { 1, 2, 3, 4 });
+
+public class Solution
+{
+    public int SumOfSquares(int[] nums)
+    {
+        int res = 0;
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if (nums.Length % (i + 1)== 0)
+                res += nums[i] * nums[i];
+        }
+
+        return res;
+    }
+}
