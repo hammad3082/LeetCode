b12ee3fb19422625b6c94ff1ba567ec2ce8c5856
hammad3082 <hammad3082@gmail.com>
2026-01-06 00:58:12 +0530

    FindDifference

--- 2215/Program.cs
+++ 2215/Program.cs
@@ -2,29 +2,24 @@
 {
     public IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
     {
-        IList<IList<int>> res = new List<IList<int>>();
-
         HashSet<int> hNum1 = new (nums1);
         HashSet<int> hNum2 = new(nums2);
 
-        HashSet<int> res1 = new();
-        HashSet<int> res2 = new();
+        List<int> res1 = new();
+        List<int> res2 = new();
 
-        foreach (int i in nums1)
+        foreach (int i in hNum1)
         {
             if(!hNum2.Contains(i))
                 res1.Add(i);
         }
 
-        foreach (int i in nums2)
+        foreach (int i in hNum2)
         {
             if (!hNum1.Contains(i))
                 res2.Add(i);
         }
 
-        res.Add(res1.ToList());
-        res.Add(res2.ToList());
-
-        return res;
+        return new List<IList<int>> { res1, res2 };
     }
 }
