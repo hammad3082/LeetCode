9cc46561722b2322b5a4efb554b94e01be0ba0e4
hammad3082 <hammad3082@gmail.com>
2026-01-09 01:02:20 +0530

    DestCity

---
+++ 1436/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public string DestCity(IList<IList<string>> paths)
+    {
+        HashSet<string> hasPathsToOtherCities = new HashSet<string>();
+
+        HashSet<string> uniqueCities = new HashSet<string>();
+
+        foreach (var path in paths)
+        {
+            hasPathsToOtherCities.Add(path[0]);
+            uniqueCities.Add(path[0]);
+            uniqueCities.Add(path[1]);
+        }
+
+        string destinationCity = string.Empty;
+        foreach (var city in uniqueCities)
+        {
+            if (!hasPathsToOtherCities.Contains(city))
+            {
+                destinationCity = city;
+                break;
+            }
+        }
+
+        return destinationCity;
+    }
+}
