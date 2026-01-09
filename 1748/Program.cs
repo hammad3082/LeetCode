120cbcaa55276f2aa907a483b465cd4f67ec863b
hammad3082 <hammad3082@gmail.com>
2026-01-10 01:00:00 +0530

    SumOfUnique

---
+++ 1748/Program.cs
@@ -0,0 +1,40 @@
+﻿public class Solution
+{
+    public int SumOfUnique(int[] nums)
+    {
+        int[] arr = new int[101];
+
+        foreach (int i in nums)
+            arr[i]++;
+
+        int sum = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if(arr[i] == 1)
+                sum += i;
+        }
+
+        return sum;
+    }
+    public int SumOfUnique_1(int[] nums)
+    {
+        Dictionary<int,int> keyValues = new Dictionary<int,int>();
+
+        foreach (int i in nums)
+        {
+            if (keyValues.ContainsKey(i))
+                keyValues[i] = keyValues[i] + 1;
+            else
+                keyValues[i] = 1;
+        }
+        int sum = 0;
+
+        foreach (var keyValue in keyValues.Where(x => x.Value == 1))
+        {
+            sum += keyValue.Key;
+        }
+
+        return sum;// keyValues.Where(x => x.Value == 1).Select(x => x.Key).Sum();
+    }
+}
