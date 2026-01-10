ce3bb26ce628f283947b649e50c0a54789a63e53
hammad3082 <hammad3082@gmail.com>
2026-01-10 19:32:18 +0530

    SplitWordsBySeparator

---
+++ 2788/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public IList<string> SplitWordsBySeparator(IList<string> words, char separator)
+    {
+        List<string> result = new List<string>();
+
+        foreach (string word in words)
+        {
+            var sRes = word.Split(separator);
+
+            foreach (string s in sRes)
+            {
+                if(s != "")
+                    result.Add(s);
+            }
+        }
+
+        return result;
+    }
+}
