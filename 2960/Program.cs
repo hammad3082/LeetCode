d5675ba1338b62a3b570cb73bbb59379a5a50a03
hammad3082 <hammad3082@gmail.com>
2026-01-12 23:44:06 +0530

    CountTestedDevices

---
+++ 2960/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public int CountTestedDevices(int[] batteryPercentages)
+    {
+        int res = 0;
+
+        foreach (int batteryPercentage in batteryPercentages)
+        {
+            res += batteryPercentage > res ? 1 : 0;
+        }
+
+        return res;
+    }
+}
