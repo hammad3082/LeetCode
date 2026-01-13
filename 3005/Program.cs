fa74c6075447d1dc765f33e8688aa04c7e9faf6d
hammad3082 <hammad3082@gmail.com>
2026-01-13 19:38:22 +0530

    MaxFrequencyElements

---
+++ 3005/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int MaxFrequencyElements(int[] nums)
+    {
+        int res = 0, maxFreq = 0;
+        int[] arr = new int[101];
+
+        foreach (int n in nums)
+        {
+            int freq = ++arr[n];
+            if (freq > maxFreq)
+            {
+                maxFreq = freq;
+                res = freq;
+            }
+            else if (freq == maxFreq)
+            {
+                res += freq;
+            }
+        }
+
+        return res;
+    }
+}
