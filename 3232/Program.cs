793f970dd317d6ed880322f469a1fd9edc0ed1ea
hammad3082 <hammad3082@gmail.com>
2026-01-15 00:48:19 +0530

    CanAliceWin

---
+++ 3232/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public bool CanAliceWin(int[] nums)
+    {
+        int sumOfSingle = 0, sumOfDouble = 0;
+
+        foreach (int num in nums)
+        {
+            if(num > 9)
+                sumOfSingle += num;
+            else
+                sumOfDouble += num;
+        }
+
+        return sumOfSingle != sumOfDouble;
+        //if (sumOfSingle != sumOfDouble)
+        //    return true;
+        //else 
+        //    return false;
+    }
+}
