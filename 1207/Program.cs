ca6d5c8651b2e51a597105decd8d0f96b7ee0d00
hammad3082 <hammad3082@gmail.com>
2026-01-16 00:50:12 +0530

    UniqueOccurrences

---
+++ 1207/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public bool UniqueOccurrences(int[] arr)
+    {
+        Dictionary<int, int> keyValues = new Dictionary<int, int>();
+        HashSet<int> uniqueFreq = new HashSet<int>();
+
+        foreach (int num in arr)
+            keyValues[num] = keyValues.TryGetValue(num, out int val) ? val + 1 : 1;
+
+        foreach (var freq in keyValues.Values)
+        {
+            if (uniqueFreq.Contains(freq))
+                return false;
+            else
+                uniqueFreq.Add(freq);
+        }
+
+        return true;
+    }
+}
