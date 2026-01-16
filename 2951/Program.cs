5d971125de3e426567c764ebc48fe4b065550aa1
hammad3082 <hammad3082@gmail.com>
2026-01-17 00:22:36 +0530

    FindPeaks

---
+++ 2951/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public IList<int> FindPeaks(int[] mountain)
+    {
+        var res = new List<int>();
+
+        for (int i = 1; i < mountain.Length - 1; i++)
+        {
+            if(mountain[i-1] < mountain[i] && mountain[i] > mountain[i+1])
+            {
+                res.Add(i);
+            }
+        }
+
+        return res;
+    }
+}
