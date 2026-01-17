c96a7903f2ecd0f479b81bf9c19eb1a8c67250a6
hammad3082 <hammad3082@gmail.com>
2026-01-18 00:55:43 +0530

    HalvesAreAlike

---
+++ 1704/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public bool HalvesAreAlike(string s)
+    {
+        int midIndex = s.Length / 2;
+
+        string firstHalf = s.Substring(0, midIndex);
+        string secondHalf = s.Substring(midIndex);
+
+        return CountVowels(firstHalf) == CountVowels(secondHalf);
+    }
+
+    int CountVowels(string str)
+    {
+        int count = 0;
+        HashSet<char> vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+        
+        foreach (char c in str)
+        {
+            if (vowels.Contains(c))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
