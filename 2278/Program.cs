c8836ac6da6dbe57cd6f46ea59e64bd04f5d30e9
hammad3082 <hammad3082@gmail.com>
2026-01-20 23:00:59 +0530

    PercentageLetter

---
+++ 2278/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public int PercentageLetter(string s, char letter)
+    {
+        int letterCount = 0;
+
+        //letterCount = s.Count(x => x == letter);
+        foreach (char c in s)
+        {
+            if (c == letter)
+                letterCount++;
+        }
+
+        return (letterCount * 100) / s.Length;
+    }
+}
