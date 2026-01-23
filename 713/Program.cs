c38af146f8de49ba78ba11ec58f1e6b32b4380cb
hammad3082 <hammad3082@gmail.com>
2026-01-24 00:52:21 +0530

    NumSubarrayProductLessThanK

---
+++ 713/Program.cs
@@ -0,0 +1,50 @@
+﻿Solution sol = new Solution();
+sol.NumSubarrayProductLessThanK(new int[] { 1, 2, 3 }, 0);
+
+public class Solution
+{
+    public int NumSubarrayProductLessThanK(int[] nums, int k)
+    {
+        if (k <= 1)
+            return 0;
+
+        int res = 0;
+        int left = 0;
+        int subProd = 1;
+
+        for (int right = 0; right < nums.Length; right++)
+        {
+            subProd *= nums[right];
+
+            while(subProd >= k)
+            {
+                subProd /= nums[left];
+                left++;
+            }
+
+            res += right - left + 1;
+        }
+
+        return res;
+    }
+    public int NumSubarrayProductLessThanK_1(int[] nums, int k)
+    {
+        int res = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int subProd = 1;
+            for (int j = i; j < nums.Length; j++)
+            {
+                subProd *= nums[j];
+
+                if (subProd < k)
+                    res++;
+                else
+                    break;
+            }
+        }
+
+        return res;
+    }
+}
