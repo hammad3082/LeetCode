19537bcd1ff079e36ac3e7c722379b637f4e5e22
hammad3082 <hammad3082@gmail.com>
2026-01-27 23:34:00 +0530

    AreOccurrencesEqual

---
+++ 1941/Program.cs
@@ -0,0 +1,23 @@
+﻿Solution sol =  new Solution();
+sol.AreOccurrencesEqual("abacbc");
+
+public class Solution
+{
+    public bool AreOccurrencesEqual(string s)
+    {
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+
+        foreach (char c in s)
+            dict[c] = dict.TryGetValue(c, out int val) ? ++val : 1;
+
+        int tfreq = dict.First().Value;
+
+        foreach (var freq in dict.Values)
+        {
+            if (tfreq != freq)
+                return false;
+        }
+
+        return true;
+    }
+}
