0feb4b33bb376e4a2241708a03a563c9fc21014f
hammad3082 <hammad3082@gmail.com>
2026-01-31 01:01:25 +0530

    FizzBuzz

---
+++ 412/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public IList<string> FizzBuzz(int n)
+    {
+        IList<string> result = new List<string>();
+
+        for (int count = 1; count <= n; count++)
+        {
+            if (count % 3 == 0 && count % 5 == 0)
+            {
+                result.Add("FizzBuzz");
+            }
+            else if (count % 5 == 0)
+            {
+                result.Add("Buzz");
+            }
+            else if (count % 3 == 0)
+            {
+                result.Add("Fizz");
+            }
+            else
+            {
+                result.Add(count.ToString());
+            }
+        }
+
+        return result;
+    }
+}
