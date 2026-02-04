88b83224096bddf7e2f675e21866920c4152f22f
hammad3082 <hammad3082@gmail.com>
2026-02-05 00:25:36 +0530

    CountKConstraintSubstrings

---
+++ 3258/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int CountKConstraintSubstrings(string s, int k)
+    {
+        int n = s.Length;
+        int res = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int zeroCount = 0, oneCount = 0;
+
+            for (int j = i; j < n; j++)
+            {
+                if (s[j] == '0')
+                    zeroCount++;
+                else
+                    oneCount++;
+
+                if (zeroCount <= k || oneCount <= k)
+                    res++;
+            }
+        }
+
+        return res;
+    }
+}
