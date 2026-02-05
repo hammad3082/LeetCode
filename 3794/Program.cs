d01e5c1db96be0a68b680039ebee157176867dc5
hammad3082 <hammad3082@gmail.com>
2026-02-06 00:45:21 +0530

    ReversePrefix

---
+++ 3794/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public string ReversePrefix(string s, int k)
+    {
+        char[] arr = s.ToCharArray();
+
+        for (int i = 0; i < k / 2; i++)
+        {
+            char temp = arr[i];
+            arr[i] = arr[k - 1 - i];
+            arr[k - 1 - i] = temp;
+        }
+
+        return new string(arr);
+    }
+}
