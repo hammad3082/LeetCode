ad3e7bb0a976b55b289aba47ce40d046cfe7c534
hammad3082 <hammad3082@gmail.com>
2026-02-07 00:55:13 +0530

    AreNumbersAscending

---
+++ 2042/Program.cs
@@ -0,0 +1,23 @@
+﻿Solution sol = new Solution();
+sol.AreNumbersAscending("1 box has 3 blue 4 red 6 green");
+public class Solution
+{
+    public bool AreNumbersAscending(string s)
+    {
+        string[] parts = s.Split(' ');
+        int prev = -1;
+
+        foreach (var part in parts)
+        {
+            if (int.TryParse(part, out int num))
+            {
+                if (num <= prev)
+                    return false;
+
+                prev = num;
+            }
+        }
+
+        return true;
+    }
+}   
