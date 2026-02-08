9eaea95041e23623b465d553514ad4a328309792
hammad3082 <hammad3082@gmail.com>
2026-02-09 00:30:44 +0530

    LargestEven

---
+++ 3798/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public string LargestEven(string s)
+    {
+        int i;
+
+        for (i = s.Length - 1; i >= 0; i--)
+        {
+            if (s[i] == '2') 
+                break;
+        }
+
+        return s.Substring(0, i + 1);
+    }
+}
