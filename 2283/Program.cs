a312cb22d5fc1b795cb7932ba11ab74de16b24f3
hammad3082 <hammad3082@gmail.com>
2026-02-09 22:54:12 +0530

    DigitCount

---
+++ 2283/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public bool DigitCount(string num)
+    {
+        int[] arr = new int[10];
+
+        for (int i = 0; i < num.Length; i++)
+            arr[num[i] - '0']++;
+
+        for (int i = 0; i < num.Length; ++i)
+        {
+            if (arr[i] != num[i] - '0')
+                return false;
+        }
+
+        return true;
+    }
+}
