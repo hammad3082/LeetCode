ead4fb24e631361fe6f37e3ce49938df6fcdb050
hammad3082 <hammad3082@gmail.com>
2026-02-11 00:00:32 +0530

    MakeFancyString

---
+++ 1957/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+public class Solution
+{
+    public string MakeFancyString(string s)
+    {
+        StringBuilder res = new StringBuilder();
+
+        char prev = s[0];
+        int freq = 1;
+
+        res.Append(prev);
+
+        for (int i = 1; i < s.Length; i++)
+        {
+            char ch = s[i];
+
+            if (ch == prev)
+                freq++;
+            else
+            {
+                prev = ch;
+                freq = 1;
+            }
+
+            if (freq < 3)
+                res.Append(ch);
+        }
+
+        return res.ToString();
+    }
+}
