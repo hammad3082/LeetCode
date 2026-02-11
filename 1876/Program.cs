cc23a2059c761fbe933be45e4ac7b6b52871191b
hammad3082 <hammad3082@gmail.com>
2026-02-12 00:25:57 +0530

    CountGoodSubstrings

---
+++ 1876/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public int CountGoodSubstrings(string s)
+    {
+        int res = 0;
+
+        for (int i = 1; i < s.Length - 1; i++)
+        {
+            if (s[i] != s[i - 1] && s[i] != s[i + 1] && s[i - 1] != s[i + 1])
+                res++;
+        }
+
+        return res;
+    }
+}
