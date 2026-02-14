15f050b6efbe9e45666732ce7a28744dc9722497
hammad3082 <hammad3082@gmail.com>
2026-02-14 23:48:17 +0530

    CountPrefixes

---
+++ 2255/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public int CountPrefixes(string[] words, string s)
+    {
+        int count = 0;
+        foreach (var x in words)
+        {
+            if (x.Length > s.Length) continue;
+            if (s.Substring(0, x.Length) == x)
+            {
+                count = count + 1;
+            }
+        }
+
+        return count;
+    }
+    public int CountPrefixes_1(string[] words, string s)
+    {
+        int res = 0;
+
+        foreach (var word in words) 
+        {
+            if(s.StartsWith(word))
+                res++;
+        }
+
+        return res;
+    }
+}
