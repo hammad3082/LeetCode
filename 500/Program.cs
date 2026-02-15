395b1d037fa58cd5b7193b458a66dfc8aa5b97d0
hammad3082 <hammad3082@gmail.com>
2026-02-16 00:59:40 +0530

    FindWords

---
+++ 500/Program.cs
@@ -0,0 +1,31 @@
+﻿Solution sol = new Solution();
+
+sol.FindWords(new string[] { "Hello", "Alaska", "Dad", "Peace" });
+public class Solution
+{
+    public string[] FindWords(string[] words)
+    {
+        List<string> res = new List<string>();
+
+        foreach (string word in words)
+        {
+            if (canBeTypedUsingRow(word, "qwertyuiopQWERTYUIOP") || canBeTypedUsingRow(word, "asdfghjklASDFGHJKL") || canBeTypedUsingRow(word, "zxcvbnmZXCVBNM"))
+            {
+                res.Add(word);
+            }
+        }
+
+        return res.ToArray();
+    }
+
+    private bool canBeTypedUsingRow(string word, string row)
+    {
+        foreach (char c in word)
+        {
+            if (row.IndexOf(c) == -1)
+                return false;
+        }
+
+        return true;
+    }
+}
