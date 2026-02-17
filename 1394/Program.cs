768a56743c5b2654cebabbbda066aa463da77f05
hammad3082 <hammad3082@gmail.com>
2026-02-18 00:53:38 +0530

    FindLucky

---
+++ 1394/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int FindLucky(int[] arr)
+    {
+        int[] numbers = new int[501];
+
+        foreach (int a in arr)
+        {
+            numbers[a]++;
+        }
+
+        for (int i = 500; i > 0; i--)
+        {
+            if (i == numbers[i]) 
+                return i;
+        }
+
+        return -1;
+    }
+}
