fe1bfa9557d0aa83f0bff99787174288ac827d5b
hammad3082 <hammad3082@gmail.com>
2026-02-19 00:27:38 +0530

    MinimumLength

---
+++ 3223/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int MinimumLength(string s)
+    {
+        int[] charFrequency = new int[26];
+        int res = 0;
+
+        foreach (char c in s)
+            charFrequency[c - 'a']++;
+
+        foreach (int frequency in charFrequency)
+        {
+            if (frequency == 0) 
+                continue;
+
+            if (frequency % 2 == 0)
+                res += 2;
+            else
+                res += 1;
+        }
+
+        return res;
+    }
+}
