622e4365bf50fcd6f4c9b3710a0d9df09557390f
hammad3082 <hammad3082@gmail.com>
2026-02-20 00:47:26 +0530

    CountGoodRectangles

---
+++ 1725/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int CountGoodRectangles(int[][] rectangles)
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+
+        int maxLen = 0;
+
+        foreach (int[] r in rectangles)
+        {
+            int side = Math.Min(r[0], r[1]);
+
+            dict.TryAdd(side, 0);
+            dict[side]++;
+
+            maxLen = Math.Max(maxLen, side);
+        }
+
+        return dict[maxLen];
+    }
+}
