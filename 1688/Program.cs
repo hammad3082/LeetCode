ef4c133be22161ffe53b9f366ee564104f42a887
hammad3082 <hammad3082@gmail.com>
2026-02-21 00:45:40 +0530

    NumberOfMatches

---
+++ 1688/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int NumberOfMatches(int n)
+    {
+        return n - 1;
+    }
+    public int NumberOfMatches_1(int n)
+    {
+        int answ = 0;
+        while (n > 1)
+        {
+            answ += n / 2;
+            if (n % 2 == 0)
+            {
+                n /= 2;
+            }
+            else
+            {
+                n = n / 2 + 1;
+            }
+        }
+        return answ;
+    }
+}
