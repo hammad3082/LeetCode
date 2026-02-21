87601691cfcb46395cb42b8b5434742752dbf66f
hammad3082 <hammad3082@gmail.com>
2026-02-22 00:11:11 +0530

    FindClosest

---
+++ 3516/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public int FindClosest(int x, int y, int z)
+    {
+        int dx = Math.Abs(x - z);
+        int dy = Math.Abs(y - z);
+        if (dx == dy)
+            return 0;
+        else if (dx < dy)
+            return 1;
+        else
+            return 2;
+    }
+}
