56c2810e5f7fb420ec58534e364e004a1d6893dd
hammad3082 <hammad3082@gmail.com>
2026-02-24 00:13:03 +0530

    CountPairs

---
+++ 2824/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int CountPairs(IList<int> nums, int target)
+    {
+        int count = 0;
+        int left = 0;
+        int right = nums.Count - 1;
+
+        nums = nums.OrderBy(x => x).ToList();
+
+        while (left < right)
+        {
+            if (nums[left] + nums[right] < target)
+            {
+                count += right - left;
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return count;
+    }
+}
