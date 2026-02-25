d3a6887cbb164783a30bdeb6cb11beadb30d6413
hammad3082 <hammad3082@gmail.com>
2026-02-26 00:00:34 +0530

    MaxScore

---
+++ 1423/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int MaxScore(int[] cardPoints, int k)
+    {
+        int len = cardPoints.Length;
+
+        int currentKSum = 0;
+
+        for (int i = 0; i < k; i++) 
+            currentKSum += cardPoints[i];
+
+        int maxKSum = currentKSum;
+
+        for (int i = 1; i <= k; i++)
+        {
+            currentKSum -= cardPoints[k - i];
+            currentKSum += cardPoints[len - i];
+
+            maxKSum = Math.Max(maxKSum, currentKSum);
+        }
+
+        return maxKSum;
+    }
+}
