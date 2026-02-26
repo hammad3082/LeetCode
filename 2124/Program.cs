1b15297640334796d7200cb1336cbc9794a05fc5
hammad3082 <hammad3082@gmail.com>
2026-02-27 00:28:29 +0530

    CheckString

---
+++ 2124/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public bool CheckString(string s)
+    {
+        bool checkForBsNow = false;
+        foreach (char c in s)
+        {
+            if(c == 'b')
+                checkForBsNow = true;
+
+            if(checkForBsNow)
+                if(c == 'a')
+                    return false;
+        }
+
+        return true;
+    }
+}
