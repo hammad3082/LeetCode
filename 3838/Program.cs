93b53ee5566c27b9eb9bbf0bca41873de6cfefd9
hammad3082 <hammad3082@gmail.com>
2026-03-01 00:52:26 +0530

    MapWordWeights

---
+++ 3838/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+public class Solution
+{
+    public string MapWordWeights(string[] words, int[] weights)
+    {
+        StringBuilder ans = new StringBuilder();
+        foreach (var s in words)
+        {
+            int cnt = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                cnt += weights[s[i] - 'a'];
+            }
+            ans.Append((char)('a' + (25 - (cnt % 26))));
+        }
+        return ans.ToString();
+    }
+}
