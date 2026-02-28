44610f6a544812fdd30b88d8e9ac79d33c6120c3
hammad3082 <hammad3082@gmail.com>
2026-03-01 00:52:41 +0530

    MaximumValue

---
+++ 2496/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public int MaximumValue(string[] strs)
+    {
+        int max = 0;
+
+        foreach (string str in strs)
+        {
+            if(int.TryParse(str, out int val))
+                max = Math.Max(max, val);
+            else
+                max = Math.Max(max, str.Length);
+        }
+
+        return max;
+    }
+}
