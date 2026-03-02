e6b99186499cbbe67058477fdc06c8ee2f3a7426
hammad3082 <hammad3082@gmail.com>
2026-03-03 01:46:14 +0530

    MinimumAverage

---
+++ 3194/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Solution
+{
+    public double MinimumAverage(int[] nums)
+    {
+        double res = 100;
+
+        Array.Sort(nums);
+
+        for (int i = 0; i < nums.Length / 2; ++i)
+        {
+            res = Math.Min(res, nums[i] + nums[nums.Length - i - 1]);
+        }
+
+        return res / 2;
+    }
+}
