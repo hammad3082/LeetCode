1400065ee206d66ac386750f03e60972116500b5
hammad3082 <hammad3082@gmail.com>
2026-03-04 01:02:40 +0530

    MinimumBoxes

---
+++ 3074/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int MinimumBoxes(int[] apple, int[] capacity)
+    {
+        int count = 0;
+        int totalApples = 0;
+
+        foreach (int a in apple)
+            totalApples += a;
+
+        var ordCapacity = capacity.OrderByDescending(x => x);
+
+        foreach (int cap in ordCapacity)
+        {
+            totalApples -= cap;
+            count++;
+
+            if (totalApples <= 0)
+                break;
+        }
+
+        return count;
+    }
+}
