f509ed8fcc0fb7cdd941957af144204e92783e07
hammad3082 <hammad3082@gmail.com>
2026-03-05 00:48:28 +0530

    SquareIsWhite

---
+++ 1812/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Solution
+{
+    public bool SquareIsWhite(string coordinates)
+    {
+        char column = coordinates[0];
+        int row = coordinates[1] - '0';
+
+        return ((column - 'a') + row) % 2 != 1;
+    }
+}
