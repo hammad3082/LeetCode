0ef46182ae6f654e7a2ff855e7d5fc1e1b414cb3
hammad3082 <hammad3082@gmail.com>
2026-03-07 00:57:40 +0530

    MinimumChairs

---
+++ 3168/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int MinimumChairs(string s)
+    {
+        int res = 0;
+
+        int currentPeopleInRoom = 0;
+
+        foreach (var evet in s)
+        {
+            if (evet == 'E')
+                currentPeopleInRoom++;
+            else
+                currentPeopleInRoom--;
+
+            res = Math.Max(res, currentPeopleInRoom);
+        }
+
+        return res;
+    }
+}
