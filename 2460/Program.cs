ae0eb4c9931dc80a21aef5f7eb9763e8af057892
hammad3082 <hammad3082@gmail.com>
2026-03-08 00:39:03 +0530

    ApplyOperations

---
+++ 2460/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    public int[] ApplyOperations(int[] nums)
+    {
+        int[] result = new int[nums.Length];
+        int count = 0;
+        int i;
+
+        for (i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] != 0)
+            {
+                if (nums[i] == nums[i + 1])
+                {
+                    result[count] = nums[i] * 2;
+                    i++;
+                }
+                else
+                    result[count] = nums[i];
+
+                count++;
+            }
+        }
+
+        if (i != nums.Length)
+            result[count] = nums[nums.Length - 1];
+
+        return result;
+    }
+}
