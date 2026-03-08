9305ae5874e51a60250498f23ef5038415d88707
hammad3082 <hammad3082@gmail.com>
2026-03-09 00:30:17 +0530

    SmallestIndex

---
+++ 3550/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public int SmallestIndex(int[] nums)
+    {
+        int n = nums.Length;
+
+        for (var i = 0; i < n; ++i)
+        {
+            var num = nums[i];
+            var sum = 0;
+
+            while (num != 0)
+            {
+                sum += num % 10;
+
+                if (sum > i)
+                    break;
+                num /= 10;
+            }
+
+            if (sum == i)
+                return i;
+        }
+
+        return -1;
+    }
+}
