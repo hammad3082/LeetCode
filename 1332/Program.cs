cbeea47da214dd8324f5244fb9da4451d15f831b
hammad3082 <hammad3082@gmail.com>
2026-03-10 01:13:19 +0530

    RemovePalindromeSub

---
+++ 1332/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public int RemovePalindromeSub(string s)
+    {
+        int left = 0, right = s.Length - 1;
+
+        while (left < right)
+        {
+            if (s[left] != s[right]) 
+                return 2;
+
+            left++;
+            right--;
+        }
+
+        return 1;
+    }
+}
