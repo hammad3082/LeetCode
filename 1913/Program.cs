8dbc9c4c58cb0b516c485fd56884762349adac81
hammad3082 <hammad3082@gmail.com>
2026-03-11 01:21:57 +0530

    MaxProductDifference

---
+++ 1913/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Solution
+{
+    public int MaxProductDifference(int[] nums)
+    {
+        int FirstMax = int.MinValue;
+        int SecondMax = FirstMax;
+
+        int FirstMin = int.MaxValue;
+        int SecondMin = FirstMin;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (FirstMax < nums[i])
+            {
+                SecondMax = FirstMax;
+                FirstMax = nums[i];
+            }
+            else if (nums[i] > SecondMax)
+                SecondMax = nums[i];
+
+            if (FirstMin > nums[i])
+            {
+                SecondMin = FirstMin;
+                FirstMin = nums[i];
+            }
+            else if (nums[i] < SecondMin)
+                SecondMin = nums[i];
+        }
+
+        return (FirstMax * SecondMax) - (FirstMin * SecondMin);
+    }
+}
