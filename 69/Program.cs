e3c1d01a7f0ebffd72560a8b3ad5a4d5471f8931
hammad3082 <hammad3082@gmail.com>
2026-03-12 00:27:18 +0530

    MySqrt

---
+++ 69/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Solution
+{
+    public int MySqrt(int x)
+    {
+        if (x < 2) 
+            return x;
+
+        int left = 1, right = x / 2;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            long square = (long)mid * mid;
+
+            if (square == x)
+            {
+                return mid;
+            }
+            else if (square < x)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return right;
+    }
+    public int MySqrt_1(int x)
+    {
+        if (x < 2)
+            return x;
+
+
+        int i = 2;
+
+        while (i * i <= x)
+            i++;
+
+
+        return i - 1;
+    }
+}
