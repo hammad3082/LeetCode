d5c651d64931fc2aea5fd13e241651063cb0515f
hammad3082 <hammad3082@gmail.com>
2026-03-13 00:16:23 +0530

    TargetIndices

---
+++ 2089/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public IList<int> TargetIndices(int[] nums, int target)
+    {
+        int count = 0, lessthan = 0;
+        List<int> res = new();
+
+        foreach (int n in nums)
+        {
+            if (n == target) 
+                count++;
+
+            if (n < target) 
+                lessthan++;
+        }
+
+        for (int i = 0; i < count; i++)
+        {
+            res.Add(lessthan++);
+        }
+
+        return res;
+    }
+}
