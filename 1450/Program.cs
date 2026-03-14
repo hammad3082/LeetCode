c590c828c28073bb07a8f20a509b4d0251d29a0a
hammad3082 <hammad3082@gmail.com>
2026-03-15 00:49:10 +0530

    BusyStudent

---
+++ 1450/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public int BusyStudent(int[] startTime, int[] endTime, int queryTime)
+    {
+        int res = 0;
+        for (int i = 0; i < startTime.Length; i++)
+        {
+            if(startTime[i] <= queryTime && queryTime <= endTime[i]) 
+            { 
+                res++; 
+            }
+        }
+
+        return res; 
+    }
+}
